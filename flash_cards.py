853e8f944a355bda9240b4799d6c3d5597806c1a
Lexi Murphy <63361013+Zazilicious@users.noreply.github.com>
2025-10-14 21:28:11 -0400

    Add files via upload

--- flash_cards.py
+++ flash_cards.py
@@ -16,21 +16,35 @@ flashcards = [
     ("Compensatory model", "Clients are responsible for solving but not causing problems; counselor is teacher."),
     ("Enlightenment model", "Clients are responsible for causing problems but not solving them; counselor is authority figure."),
     
+    
     # Chapter 2
     ("Ethics", "Philosophical discipline concerned with human conduct and moral decision making."),
     ("Professional Ethics", "Beliefs about behavior and conduct that guide professional practices."),
     ("Morality", "Judgment or evaluation of an action as right, wrong, good, bad, ought, should."),
     ("Law", "The precise codification of governing standards established to ensure legal and moral justice."),
     ("Unethical behaviors", "Violating confidentiality, exceeding competency, negligent practice, false expertise, imposing values, dependency, sex with client, dual relationships, financial issues, improper advertising, plagiarism."),
+    ("ethics values in counseling", "ASERVIC (Association for Spiritual, Ethical, and Religious values in Counselin)"),
     ("ACA Ethics headings", "A) Counseling relationship B) Confidentiality C) Professional Responsibility D) Relationships with Others E) Evaluation F) Supervision G) Research H) Resolving Ethical Issues"),
     ("Six ethical principles", "Beneficence, Nonmaleficence, Autonomy, Justice, Fidelity, Veracity"),
     ("Five-stage developmental continuum", "1) Punishment 2) Institutional 3) Societal 4) Individual 5) Principle"),
+    ("How to judge actions to take:", "Publicity, justice, moral traces (feelings of doubt), universality"),
+    ("Tarasoff V. Board of Regents of the University of California (1976", "-guy in therapy said he was gonna kill his girlfriend, psychologist notified police, police did nothing, guy killed his girlfriend, parents of Tatiana Tarasoff sue the university for failing to notify the victim of a threat against her, California Supreme Court ruled in parents favor, therapists have the duty to protect the public that overrides any obligation to maintain client confidentiality."),
+    ("Bogust V. Iverson (1960)","Counselor with a doctoral degree cannot be held liable for the suicide of one of his clients because counselors were merely teachers."),
+    ("Iowa Law Review Note (1971)", "Counselors legally recognized as professionals who provided personal as well as vocational and educational counseling"),
+    ("Weldon v. Virginia State Board of Psychologists Examiners", "Counseling is a professional distinct from psychology"),
+    ("Liability", "have counselors caused harm to clients"),
+    ("Malpractice", "harm to client caused by negligence"),
+    ("Negligence", "departure from acceptable professional standards"),
+    ("3 ways to protect oneself from malpractice)", "a) follow professional codes of ethics, b) follow normal practice standards, c) act professionally, attentively, and courteously."),
+    ("Liability", "legal responsibility for something"),
+    
     
     # Chapter 3
     ("Self-awareness", "Developing self-awareness from the inside out."),
     ("Awareness of others", "Developing awareness of others from the outside in."),
     
     # Chapter 4
+    ("ADAA", "Association for Adult Development and Aging"),
     ("Development", "Systematic change that is lifelong and cumulative."),
     ("Young-Old", "65-75 years"),
     ("Old", "75+ years"),
@@ -39,6 +53,7 @@ flashcards = [
     ("Sexism", "Belief that females should be treated based on sex, ignoring other criteria"),
     ("Pink-color jobs", "Jobs that primarily employ women"),
     ("Feminist theory", "Focuses on female development as persons with common and unique qualities"),
+    ("ALGBTIC", "Association for Lesbian, Gay, Bisexual, and Transgender Issues in Counseling (ALGBTIC)"),
     ("SEM", "Social Empowerment Model: increases collective and personal self-advocacy for LGBTQ+"),
     ("GRC", "Gender Role Conflict: empowers trans people to live life on their own terms"),
     ("ADDRESSING Model", "A- age, D- developmental disabilities, D- acquired disabilities, R- religion, E- ethnicity, S- social status, S- sexual orientation, I- indigenous heritage, N- national origin, G- gender"),
@@ -47,10 +62,30 @@ flashcards = [
     ("Battle for structure", "Issues of administrative control (scheduling, fees, etc.)"),
     ("Battle for initiative", "Motivation for change and client responsibility"),
     ("Microskills", "Counselor responses that cut across cultural and theoretical lines"),
+    ("Structure", "Practical guidelines (time limits, action limits, role limits, and procedural limits), Professional disclosure statements (include details about the nature of counseling, expectations, responsibilities, methods, and ethics of counseling)"),
+    ("Initiative", "Motivation to change"),
+    ("Problems of initiative", "Reluctant client, Resistant Client"),
+    ("Direct persuasion techniques", "Foot-in-the-door, Door-in-the-face"),
+    ("Confrontation", "the counselor points out to the client what the client is doing, i.e.inconsistencies."),
+    ("Metaphors", "can be used to teach and reduce threat levels"),
+    ("mattering", "the belief that as humans we are important and significant to the world around us and the lives of others"),
+    ("Eight common characteristics of space and their potential impact", "Accessories, Color, Furniture and room design, Lighting, Smell, Sound, Texture, Thermal conditions"),
+    ("Proxemics", "the distance between counselor and client; Average range of comfort is between 30-39 inches, with chairs set at a 90 degree angle"),
     ("YAVIS", "Young, Attractive, Verbal, Intelligent, Successful clients (more successful)"),
     ("HOUNDs", "Homely, Old, Unintelligent, Nonverbal, Disadvantaged clients (less successful)"),
+    ("Informed-oriented first interview", "Includes probes, closed questions, requests for clarification"),
+    ("Relationship-oriented fist interview", "Includes restatement, reflection of feelings, summary of feelings, and acknowledgement of nonverbal behavior"),
+    ("Rapport", "genuine interest in and accepting of the client"),
+    ("Two important micro skills for rapport building are:", "-Attending behavior, -client-observation skills"),
+    ("Attentiveness", "the amount of verbal and nonverbal behavior shown to the client"),
+    ("5 nonverbal skills involve initial attending, known as SOLER:", "Face the client Squarely, Adopt an Open posture, -Lean toward the client, -Appropriate Eye contact,Relax and be comfortable "),
+    ("Four major actions that usually block the counselor-client communication and should be generally avoided:", "-Advice-giving,-Lecturing, -Excessive questioning,  -Storytelling by the counselor"),
+    ("Unfocused goals", " not identified, too broad, or not prioritized"),
+    ("Unrealistic goals", "may include happiness, perfection, progress, being number one, and self-actualization. They have merit but are not easily obtained or sustained."),
+    ("Uncoordinated goals", "all into either really uncoordinated (incompatibility between goals and client) or seemingly uncoordinated (appear to be uncoordinated but really are not)"),
+    ("Dryer and Vriend (1977) 7 specific criteria for", "MSRAQBBU: Mutually Agreed on , Specific, Relevant to self-defeating behavior, achievement oriented, quantifiable and measurable, behavioral and observable, understandable and cab be restated clearly"),
     ("Empathy", "Counselor’s ability to enter the client’s world without losing 'as if' quality"),
-    ("SBIRT", "Screening, Brief Intervention, Referral to Treatment model for substance use disorders")
+    ("SBIRT", "Screening, Brief Intervention, Referral to Treatment model for substance use disorders"),
 ]
 
 current_index = 0
